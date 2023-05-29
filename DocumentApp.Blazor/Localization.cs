e2bf45d34bf0eca50546eb70293401132c05a250
waffencode <waffencode@gmail.com>
2023-05-29 08:10:18 +0300

    Add russian translation for enum elements

---
+++ DocumentApp.Blazor/Localization.cs
@@ -0,0 +1,33 @@
+﻿using DocumentApp.Domain;
+
+namespace DocumentApp.Blazor
+{
+    public static class Localization
+    {
+        public static string Translate(PublicationType type)
+        {
+            switch (type)
+            {
+                case PublicationType.Article: return "Статья";
+                case PublicationType.Textbook: return "Учебник";
+                case PublicationType.Thesis: return "Доклад";
+                case PublicationType.Report: return "Отчёт";
+                case PublicationType.Monography: return "Монография";
+                default: return "Неизвестно";
+            }
+        }
+
+        public static string Translate(PublicationStatus status)
+        {
+            switch (status)
+            {
+                case PublicationStatus.Draft: return "Черновик";
+                case PublicationStatus.Sent: return "Отправлено в редакцию";
+                case PublicationStatus.Finalizing: return "На доработке";
+                case PublicationStatus.Accepted: return "Принято";
+                case PublicationStatus.Published: return "Опубликовано";
+                default: return "Неизвестно";
+            }
+        }
+    }
+}
